b6a8225cee0d5d48bcd4ba3c6d8588d0ea7ce2c9
György Kurucz <me@kuruczgy.com>
2024-09-07 16:50:06 +0200

    Move kernel into a separate package

--- flake.nix
+++ flake.nix
@@ -39,6 +39,7 @@
 
       overlays = [
         (final: prev: {
+          x1e80100-linux = final.callPackage ./packages/x1e80100-linux.nix { };
           x1e80100-lenovo-yoga-slim7x-firmware = final.callPackage ./packages/x1e80100-lenovo-yoga-slim7x-firmware.nix { };
           x1e80100-lenovo-yoga-slim7x-firmware-json = final.callPackage ./packages/x1e80100-lenovo-yoga-slim7x-firmware-json.nix { };
           libqrtr = final.callPackage ./packages/libqrtr.nix { };
