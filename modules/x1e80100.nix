a36befe6bba0b771cd2cf3b4eb16b7bf7e81dfd1
György Kurucz <me@kuruczgy.com>
2024-09-07 22:49:32 +0200

    Remove some unnecessary initrd modules and add panel driver
    The ISO should be reproducible and have the following SHA256 hash:
    0c5967faa6ab0d1314b6bc7e5101dbaaa4b28ab9ff2cd56ebadbb8b3ba9022ea

--- modules/x1e80100.nix
+++ modules/x1e80100.nix
@@ -1,79 +1,7 @@
 { pkgs, lib, ... }:
 
-let
-  # Copied from:
-  # https://github.com/colemickens/nixos-snapdragon-elite/blob/c4817fe8609690350a01513ebc851a393baaae50/snapdragon.nix#L50
-  colemickens_initrd_modules = [
-    # Make sure the initramfs includes any modules required to boot, for
-    # example:
-    # 	nvme phy_qcom_qmp_pcie pcie_qcom
-    "nvme"
-    "phy_qcom_qmp_pcie"
-    "pcie_qcom"
-
-    # for the X13s and x1e80100-crd, and
-    # phy_qcom_qmp_ufs ufs_qcom
-    "phy_qcom_qmp_ufs"
-    "ufs_qcom"
-
-    # for the sc8280xp-crd with rootfs on UFS.
-
-    # For keyboard input and (more than 30 seconds of) display in initramfs,
-    # make sure to also include:
-
-    # 	i2c_hid_of i2c_qcom_geni
-    "i2c_hid_of"
-    "i2c_qcom_geni"
-
-    # for keyboard, and
-
-    # 	leds_qcom_lpg pwm_bl
-    # 	qrtr pmic_glink_altmode gpio_sbu_mux phy_qcom_qmp_combo
-    # 	gpucc_sc8280xp dispcc_sc8280xp
-    # 	phy_qcom_edp panel_edp msm
-
-    "leds_qcom_lpg"
-    "pwm_bl"
-    "qrtr"
-    "pmic_glink_altmode"
-    "gpio_sbu_mux"
-    "phy_qcom_qmp_combo"
-    "gpucc_sc8280xp"
-    "dispcc_sc8280xp"
-    "dispcc-x1e80100"
-    "gpucc-x1e80100"
-    "tcsrcc-x1e80100"
-    "phy_qcom_edp"
-    "panel_edp"
-    "msm"
-
-    "phy-qcom-qmp-usb"
-    "phy-qcom-qmp-usbc"
-    "phy-qcom-usb-hs"
-    "phy-qcom-usb-hsic"
-    "phy-qcom-usb-ss"
-    "qcom_pmic_tcpm"
-    "qcom_usb_vbus-regulator"
-
-    # for the display.
-
-    # doh, duh
-    "uas"
-
-    # random tries:
-    "r8152"
-    "lzo_rle"
-    "msm"
-
-    # ??
-    "dwc3-qcom"
-
-    # "another one"
-    "evdev"
-  ];
-in
 {
-  boot.initrd.availableKernelModules = lib.mkForce ([
+  boot.initrd.availableKernelModules = lib.mkForce [
     # Needed by the NixOS iso for booting in general
     "squashfs"
     "iso9660"
@@ -87,24 +15,17 @@ in
     "phy_qcom_eusb2_repeater"
     "tcsrcc_x1e80100"
 
-    # From jhovold defconfig commit msg:
-    "leds_qcom_lpg"
-    "pwm_bl"
-    "qrtr"
-    "pmic_glink_altmode"
-    "gpio_sbu_mux"
-    "phy_qcom_qmp_combo"
-    "gpucc_sc8280xp"
-    "dispcc_sc8280xp"
+    "i2c_hid_of"
+    "i2c_qcom_geni"
+    "dispcc-x1e80100"
+    "gpucc-x1e80100"
     "phy_qcom_edp"
     "panel_edp"
     "msm"
-  ] ++ colemickens_initrd_modules);
-
-  boot.initrd.kernelModules = [
-    "i2c_hid"
-    "i2c_hid_of"
-    "i2c_qcom_geni"
+    "nvme"
+    "phy_qcom_qmp_pcie"
+    "pcie_qcom"
+    "panel_samsung_atna33xc20"
   ];
 
   boot.blacklistedKernelModules = [ "qcom_edac" ];
