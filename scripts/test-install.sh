f8ea09336cfe0ceab2179bb1857665f2e674828a
György Kurucz <me@kuruczgy.com>
2024-09-10 13:25:59 +0200

    Add test-install.sh script

---
+++ scripts/test-install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# This script can be used to test the installer quickly. To use it, you need
+# the following:
+# - create an EFI partition with the filesystem label "TESTING-EFI"
+# - create a root partition with the partition label "testing-root"
+# To install, connect to Wi-Fi, then run this script as
+# `sudo /x1e-nixos-config/scripts/test-install.sh`.
+
+set -ex
+
+if ! mountpoint /mnt; then
+    yes | mkfs.ext4 -L root /dev/disk/by-partlabel/testing-root
+    mount /dev/disk/by-label/root /mnt
+    mkdir -p /mnt/boot
+    mount /dev/disk/by-label/TESTING-EFI /mnt/boot
+fi
+nixos-install --root /mnt --no-channel-copy --no-root-password --flake $(readlink /x1e-nixos-config)#system
