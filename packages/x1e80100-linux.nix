efb6097f46cf32b0278116d79bcce94186d83d6b
György Kurucz <me@kuruczgy.com>
2024-09-22 18:07:03 +0200

    Update kernel to 6.11
    The ISO should be reproducible and have the following SHA256 hash:
    789a25275226e35a18f103dc431d5f1317d033d6de9874e42ac239a53210deca

--- packages/x1e80100-linux.nix
+++ packages/x1e80100-linux.nix
@@ -4,10 +4,10 @@ linuxPackagesFor (buildLinux {
   src = fetchFromGitHub {
     owner = "jhovold";
     repo = "linux";
-    rev = "wip/x1e80100-6.11-rc7";
-    hash = "sha256-p5DcTD5Vt1ME3jb9d+QPBjoOpbpV371sucK7bb+V3JA=";
+    rev = "wip/x1e80100-6.11";
+    hash = "sha256-yQR1N3WDuKV1X3ph4wYXSFk5kVMbUDwvG4s8AjmpmnU=";
   };
-  version = "6.11.0-rc7";
+  version = "6.11.0";
   defconfig = "johan_defconfig";
 
   structuredExtraConfig = with lib.kernel; {
