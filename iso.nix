453b9e096ff65147e71334a05ec3ca4c241b4457
György Kurucz <me@kuruczgy.com>
2024-10-03 21:24:19 +0200

    Remove unnecessary packages from the ISO
    I originally added these to be able to test graphics and input directly
    in the ISO, but now the install process is stable and fast enough that
    these are no longer really necessary.
    
    This should also save quite a bit of cross-compiling time.

--- iso.nix
+++ iso.nix
@@ -7,17 +7,6 @@
   # For some reason the adsp booting up messes with USB boot, so disable it.
   boot.blacklistedKernelModules = [ "qcom_q6v5_pas" ];
 
-  environment.systemPackages = [
-    pkgs.kmscube
-    pkgs.mesa-demos
-    pkgs.vulkan-tools
-    pkgs.evtest
-    pkgs.sway
-    pkgs.strace
-  ];
-
-  hardware.graphics.enable = true;
-
   # Include this repo in the image
   systemd.tmpfiles.rules = [ "L /x1e-nixos-config - - - - ${./.}" ];
 }
