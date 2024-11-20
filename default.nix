70dec3b46f9fbf02374b4bb9fdbd3a2b64b3589c
György Kurucz <me@kuruczgy.com>
2024-11-20 20:11:01 +0100

    Fix sway not assigning the touchscreen to the builtin screen
    This issue is exposed when using an external monitor.
    
    The ISO should be reproducible and have the following SHA256 hash:
    32179e389330a4e6f23dacd47c6618e342e19a899bb2ac1695ebfdf71e99eee0

--- default.nix
+++ default.nix
@@ -1,11 +1,24 @@
 {
-  nixosModules.x1e = { lib, ... }: {
+  nixosModules.x1e = { lib, pkgs, ... }: {
     imports = [
       ./modules/x1e80100.nix
       ./modules/pd-mapper.nix
     ];
     config = {
-      nixpkgs.overlays = [ (import ./packages/overlay.nix) ];
+      nixpkgs.overlays = [
+        (import ./packages/overlay.nix)
+        (final: prev: {
+          sway-unwrapped = prev.sway-unwrapped.overrideAttrs (old: {
+            patches = old.patches ++ [
+              # https://github.com/swaywm/sway/pull/8469
+              (pkgs.fetchpatch {
+                url = "https://github.com/swaywm/sway/commit/eee930a4a0d3bf0de2235661227bf1a827edd320.patch";
+                hash = "sha256-1aiU4GM/OTqhJElvMecGgNbpuGbVLyhkjwC91K/eMsU=";
+              })
+            ];
+          });
+        })
+      ];
 
       hardware.deviceTree.name = lib.mkDefault "qcom/x1e80100-lenovo-yoga-slim7x.dtb";
     };
