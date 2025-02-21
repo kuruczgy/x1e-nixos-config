7b4835aece8f33e0b09666d5188426170da3e9df
György Kurucz <me@kuruczgy.com>
2025-02-21 22:45:32 +0100

    Update nixpkgs

--- default.nix
+++ default.nix
@@ -9,17 +9,6 @@
       config = {
         nixpkgs.overlays = [
           (import ./packages/overlay.nix)
-          (final: prev: {
-            sway-unwrapped = prev.sway-unwrapped.overrideAttrs (old: {
-              patches = old.patches ++ [
-                # https://github.com/swaywm/sway/pull/8469
-                (pkgs.fetchpatch {
-                  url = "https://github.com/swaywm/sway/commit/eee930a4a0d3bf0de2235661227bf1a827edd320.patch";
-                  hash = "sha256-1aiU4GM/OTqhJElvMecGgNbpuGbVLyhkjwC91K/eMsU=";
-                })
-              ];
-            });
-          })
         ];
 
         hardware.deviceTree.name = lib.mkDefault "qcom/x1e80100-lenovo-yoga-slim7x.dtb";
