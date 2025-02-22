9613a2c2269f91ab9b651707e9bc5e4a389d3755
György Kurucz <me@kuruczgy.com>
2025-02-23 00:21:24 +0100

    Remove `libqrtr`
    Apparently forgot this in 49ef274057c46d727424018f68bd6680126cb2c9

--- packages/overlay.nix
+++ packages/overlay.nix
@@ -3,6 +3,5 @@ final: prev: {
   x1e80100-lenovo-yoga-slim7x-firmware =
     final.callPackage ./x1e80100-lenovo-yoga-slim7x-firmware.nix
       { };
-  libqrtr = final.callPackage ./libqrtr.nix { };
   slbounce = final.callPackage ./slbounce.nix { };
 }
