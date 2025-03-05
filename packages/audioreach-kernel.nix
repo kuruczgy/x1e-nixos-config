dbc1faf471db620123fa84d313cd78c8908b1d9e
Aidan <aidanzcase@gmail.com>
2025-03-05 12:52:31 -0600

    Give extra patch to audioreach-kernel

--- packages/audioreach-kernel.nix
+++ packages/audioreach-kernel.nix
@@ -1,17 +1,22 @@
-{ kernel, stdenv, fetchFromGitHub}:
-let
+{
+  kernel,
+  stdenv,
+  fetchFromGitHub,
+}:
+stdenv.mkDerivation rec {
+  pname = "audioreach-linux-driver";
+  version = "1.0+git";
   src = fetchFromGitHub {
     owner = "Audioreach";
     repo = "audioreach-kernel";
     rev = "ac219fa04232a66dfdacbe0c672eef0c09a96aa0";
     hash = "sha256-ozBfRXkg0x8khmKfCYWP1B98b4OEukhACGQXttyY33c=";
   };
-in
- stdenv.mkDerivation {
-  pname = "audioreach-linux-driver";
-  version = "1.0+git";
-  inherit src;
-  hardeningDisable = ["pic" "format"];
+  patches = [ ./audioreach-proper-makefile.patch ];
+  hardeningDisable = [
+    "pic"
+    "format"
+  ];
   nativeBuildInputs = kernel.moduleBuildDependencies;
   makeFlags = kernel.makeFlags ++ [
     # KERNEL_SRC: used by audioreach-kernel
