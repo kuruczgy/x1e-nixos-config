6db8ccdd454f2a351f8edbec396ac5a161ebfaf6
Aidan <aidanzcase@gmail.com>
2025-03-05 11:46:05 -0600

    Update certain flags

--- packages/audioreach-kernel.nix
+++ packages/audioreach-kernel.nix
@@ -11,15 +11,18 @@ in
   pname = "audioreach-linux-driver";
   version = "1.0+git";
   inherit src;
-  hardeningDisable = ["pic"];
+  hardeningDisable = ["pic" "format"];
   nativeBuildInputs = kernel.moduleBuildDependencies;
   makeFlags = kernel.makeFlags ++ [
+    # KERNEL_SRC: used by audioreach-kernel
     "KERNEL_SRC=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
+    # INSTALL_MOD_PATH: used by the kbuild system
     "INSTALL_MOD_PATH=$\{out}"
   ];
   postInstall = ''
     install -d $out/include/linux
     cp -rf ${src}/include/uapi/linux/* $out/include/linux
   '';
+  buildFlags = [ "all" ];
   installTargets = [ "modules_install" ];
 }
