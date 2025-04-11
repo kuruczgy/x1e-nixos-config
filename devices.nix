d1d73366e0808871316443b862eff0be633d22af
György Kurucz <me@kuruczgy.com>
2025-04-12 00:15:29 +0200

    Add support for the ThinkPad T14s

--- devices.nix
+++ devices.nix
@@ -3,4 +3,8 @@
     displayName = "Lenovo Yoga Slim 7x";
     deviceTreeName = "qcom/x1e80100-lenovo-yoga-slim7x.dtb";
   };
+  lenovo-thinkpad-t14s = {
+    displayName = "Lenovo ThinkPad T14s Gen 6";
+    deviceTreeName = "qcom/x1e78100-lenovo-thinkpad-t14s.dtb";
+  };
 }
