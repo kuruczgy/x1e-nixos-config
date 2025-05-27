34424d86cec263964bffb9e4efceb9466f9e84fe
György Kurucz <me@kuruczgy.com>
2025-05-27 20:46:20 +0200

    Move linux-firmware downgrade to packages/overlay.nix
    It turns out the downgrade was not making it into the ISO. This is not
    the nicest solution but it should work for now.

--- default.nix
+++ default.nix
@@ -9,17 +9,6 @@
       config = {
         nixpkgs.overlays = [
           (import ./packages/overlay.nix)
-
-          (final: prev: {
-            # Downgrade linux-firmware, the ath12k firmware is broken on the latest version.
-            linux-firmware = prev.linux-firmware.overrideAttrs rec {
-              version = "20250410";
-              src = final.fetchzip {
-                url = "https://cdn.kernel.org/pub/linux/kernel/firmware/linux-firmware-${version}.tar.xz ";
-                hash = "sha256-aQdEl9+7zbNqWSII9hjRuPePvSfWVql5u5TIrGsa+Ao=";
-              };
-            };
-          })
         ];
       };
     };
--- packages/overlay.nix
+++ packages/overlay.nix
@@ -4,4 +4,13 @@ final: prev: {
     final.callPackage ./x1e80100-lenovo-yoga-slim7x-firmware.nix
       { };
   slbounce = final.callPackage ./slbounce.nix { };
+
+  # Downgrade linux-firmware, the ath12k firmware is broken on the latest version.
+  linux-firmware = prev.linux-firmware.overrideAttrs rec {
+    version = "20250410";
+    src = final.fetchzip {
+      url = "https://cdn.kernel.org/pub/linux/kernel/firmware/linux-firmware-${version}.tar.xz ";
+      hash = "sha256-aQdEl9+7zbNqWSII9hjRuPePvSfWVql5u5TIrGsa+Ao=";
+    };
+  };
 }
