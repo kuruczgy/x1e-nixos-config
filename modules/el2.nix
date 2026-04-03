3cc90ee551450a000f1546aff4513e6ca9c117e0
György Kurucz <me@kuruczgy.com>
2026-04-03 11:50:46 +0200

    Add tcblaunch.exe FOD

--- modules/el2.nix
+++ modules/el2.nix
@@ -7,16 +7,39 @@
 
 let
   cfg = config.hardware;
+
+  tcblaunch_exe = pkgs.fetchurl {
+    # Download link obtained from https://winbindex.m417z.com/?arch=arm64&file=tcblaunch.exe
+    # I don't know how trustworthy that site is, but this is a microsoft.com
+    # download link, and the expected hash for this tcblaunch.exe has been in
+    # the README of this repo for over a year prior.
+    url = "https://msdl.microsoft.com/download/symbols/tcblaunch.exe/EC74C165f6000/tcblaunch.exe";
+    # This is the hash of a tcblaunch.exe from a Windows 11 install on a Lenovo
+    # Yoga Slim 7x, has been working well with slbounce on this laptop for a
+    # while.
+    outputHash = "5dfcd0253b6ee99499ab33cac221e8a9cea47f3fdf6d4e11de9a9f3c4770d03d";
+    outputHashAlgo = "sha256";
+  };
 in
 {
-  options.x1e.el2.enable = lib.mkEnableOption ''
-    Enable the `el2` specialization and slbounce EFI driver. Needed to run
-    virtual machines using KVM.
-  '';
-
-  options.x1e.el2.qebspilFirmwareFiles = lib.mkOption {
-    type = with lib.types; listOf str;
-    description = "List of firmware files to be loaded during boot, before switching to EL2";
+  options.x1e.el2 = {
+    enable = lib.mkEnableOption ''
+      Enable the `el2` specialization and slbounce EFI driver. Needed to run
+      virtual machines using KVM.
+    '';
+    qebspilFirmwareFiles = lib.mkOption {
+      type = with lib.types; listOf str;
+      description = "List of firmware files to be loaded during boot, before switching to EL2";
+    };
+    tcblaunch = lib.mkOption {
+      type = with lib.types; nullOr pathInStore;
+      description = ''
+        The tcblaunch.exe used by slbounce to boot into EL2. Defaults to
+        downloading one from Microsoft that's know to work with the Lenovo Yoga
+        Slim 7x.
+      '';
+      default = tcblaunch_exe;
+    };
   };
 
   config = lib.mkIf config.x1e.el2.enable {
@@ -51,6 +74,7 @@ in
 
     boot.loader.systemd-boot.extraFiles = {
       "EFI/systemd/drivers/slbounceaa64.efi" = "${pkgs.slbounce}/slbounce.efi";
+      "tcblaunch.exe" = lib.mkIf (config.x1e.el2.tcblaunch != null) config.x1e.el2.tcblaunch;
       "EFI/systemd/drivers/qebspilaa64.efi" = lib.mkIf (
         config.x1e.el2.qebspilFirmwareFiles != [ ]
       ) "${pkgs.qebspil}/qebspilaa64.efi";
